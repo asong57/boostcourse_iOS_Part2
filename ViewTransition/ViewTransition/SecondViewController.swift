e4917cca52ca54ff95331e1a0b8b5a37e0e6a57e
asong57 <1998thddk@gmail.com>
2021-07-17 17:04:42 +0900

    네비게이션 인터페이스 구현해보기

---
+++ ViewTransition/ViewTransition/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  ViewTransition
+//
+//  Created by asong on 2021/07/17.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //navigation pop하는 코드
+    @IBAction func popToPrev(){
+        self.navigationController?.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
