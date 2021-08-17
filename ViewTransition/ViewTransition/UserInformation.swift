5aede503d554b3c5e857dc0c160cf8d8ff3a051d
asong57 <1998thddk@gmail.com>
2021-08-17 19:48:47 +0900

    Singleton 구현

---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,15 @@
+//
+//  UserInformation.swift
+//  ViewTransition
+//
+//  Created by asong on 2021/08/17.
+//
+
+import Foundation
+
+class UserInformation{
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var age: String?
+}
